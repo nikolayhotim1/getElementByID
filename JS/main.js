a871fa736a045b8bb3d8f242bd74f8a3c25b7f8d
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-05-28 23:13:53 +0300

    create new function

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,65 @@
 // create elements by id
-var firstNameId = "first_name";
-var firstNameEl = document.getElementById(firstNameId);
-var lastNameId = "last_name";
-var lastNameEl = document.getElementById(lastNameId);
-var addressId = "address";
-var addressEl = document.getElementById(addressId);
-var citiesId = "cities";
-var citiesEl = document.getElementById(citiesId);
-var hobbiesId = "hobbies";
-var hobbiesEl = document.getElementById(hobbiesId);
-var avatarWrapperId = "avatar_wrapper";
-var avatarWrapperEl = document.getElementById(avatarWrapperId);
-var avatarId = "avatar";
-var avatarEl = document.getElementById(avatarId);
+let firstNameId = "first_name";
+let firstNameEl = document.getElementById(firstNameId);
+let lastNameId = "last_name";
+let lastNameEl = document.getElementById(lastNameId);
+let addressId = "address";
+let addressEl = document.getElementById(addressId);
+let citiesId = "cities";
+let citiesEl = document.getElementById(citiesId);
+let hobbiesId = "hobbies";
+let hobbiesEl = document.getElementById(hobbiesId);
+let avatarWrapperId = "avatar_wrapper";
+let avatarWrapperEl = document.getElementById(avatarWrapperId);
+let avatarId = "avatar";
+let avatarEl = document.getElementById(avatarId);
+
+function consoleLogValue() {
+    console.log(firstNameEl.value);
+    console.log(lastNameEl.value);
+    console.log(addressEl.value);
+    console.log(citiesEl.value);
+    console.log(hobbiesEl.value);
+    console.log(avatarWrapperEl.innerHTML);
+}
+
 //change elements
 firstNameEl.value = "Matvey";
-lastNameEl.className = "last_name_input default_input error_input";
-lastNameEl.title = "Wrong last name";
-avatarEl.src = "img/4.jpg";
-avatarEl.title = "I am avatar";
-citiesEl.value = "Minsk";
-hobbiesEl.value = "Toys, bathing, cars";
-avatarWrapperEl.innerHTML = "<ul id=\"list\"><li>1</li><li>2</li><li>3</li></ul>";
-//create new elements for change existing inner HTML
-var listId = "list";
-var listEl = document.getElementById(listId);
 
-alert(listEl.innerHTML);
+consoleLogValue();
+
+lastNameEl.className = "last_name_input default_input error_input";
+
+consoleLogValue();
+
+lastNameEl.title = "Wrong last name";
+
+consoleLogValue();
+
+avatarEl.src = "img/4.jpg";
+
+consoleLogValue();
+
+avatarEl.title = "I am avatar";
+
+consoleLogValue();
+
+citiesEl.value = "Minsk";
+
+consoleLogValue();
+
+hobbiesEl.value = "Toys, bathing, cars";
+
+consoleLogValue();
+
+avatarWrapperEl.innerHTML = "<ul id=\"list\"><li>1</li><li>2</li><li>3</li></ul>";
+
+consoleLogValue();
+
+//create new elements for change existing inner HTML
+let listId = "list";
+let listEl = document.getElementById(listId);
+
+consoleLogValue();
+
+console.log(listEl.innerHTML);
